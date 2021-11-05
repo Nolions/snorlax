20cf6e93357af1fb058148dd0e8197446ef1ac63
bNolions <bnolions@h-tech.co>
2021-11-05 15:37:00 +0800

    [revsie] add designationID colum of vide data class

--- src/main/kotlin/com/snorlax/model/Video.kt
+++ src/main/kotlin/com/snorlax/model/Video.kt
@@ -1,8 +1,13 @@
 package com.snorlax.model
 
+import java.util.*
+
+
 data class Video(
     val name: String? = null,
     val coverPath: String? = null,
     val mediaPath: String? = null,
     val summary: String = ""
-)
+) {
+    val designationID: String = UUID.randomUUID().toString()
+}
